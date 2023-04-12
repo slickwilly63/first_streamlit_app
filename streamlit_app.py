7e2c9d8bb4b47a4446f05cfe2661c87e08de5063
slickwilly63 <64223921+slickwilly63@users.noreply.github.com>
2023-04-12 09:13:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Egg Free-Range Egg')
